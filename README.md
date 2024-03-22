e0a945aa77e038835c304436cb52e8077f7f990c
Daniel Chernenkov <danielckv@gmail.com>
2024-03-22 18:20:01 +0200

    added new files

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 ## Intro
 The project is based on RCNN approach to detect faces and masks in images.
 
-## Setup
-
-Install local dependencies:
-```bash
-pip install -e .
-```
+## Model Architecture
+- Input layer: Convolutional layer with 32 filters, kernel size 3x3, and ReLU activation function
+- Hidden layer: Max pooling layer with pool size 2x2
+- Hidden layer: Convolutional layer with 64 filters, kernel size 3x3, and ReLU activation function
+- Hidden layer: Max pooling layer with pool size 2x2
+- Hidden layer: Convolutional layer with 128 filters, kernel size 3x3, and ReLU activation function
+- Hidden layer: Max pooling layer with pool size 2x2
+- Flatten layer
+- Dense layer with 128 units and ReLU activation function
+- Output layer: Dense layer with 1 unit and sigmoid activation function
---
+++ datasets/prepare_dataset.py
@@ -0,0 +1,72 @@
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from bs4 import BeautifulSoup
+import torchvision
+from torchvision import transforms, datasets, models
+import torch
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+import matplotlib.patches as patches
+import os
+
+
+def generate_box(obj):
+    xmin = int(obj.find('xmin').text)
+    ymin = int(obj.find('ymin').text)
+    xmax = int(obj.find('xmax').text)
+    ymax = int(obj.find('ymax').text)
+
+    return [xmin, ymin, xmax, ymax]
+
+
+def generate_label(obj):
+    if obj.find('name').text == "with_mask":
+        return 1
+    elif obj.find('name').text == "mask_worn_incorrect":
+        return 2
+    return 0
+
+
+def generate_target(image_id, file):
+    with open(file) as f:
+        data = f.read()
+        soup = BeautifulSoup(data, 'xml')
+        objects = soup.find_all('object')
+        boxes = []
+        labels = []
+        for i in objects:
+            boxes.append(generate_box(i))
+            labels.append(generate_label(i))
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        img_id = torch.tensor([image_id])
+        return {"boxes": boxes, "labels": labels, "image_id": img_id}
+
+
+class MaskDataset(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir("/kaggle/input/face-mask-detection/images/")))
+        self.labels = list(sorted(os.listdir("/kaggle/input/face-mask-detection/annotations/")))
+
+    def __getitem__(self, idx):
+        # load images ad masks
+        file_image = 'maksssksksss' + str(idx) + '.png'
+        file_label = 'maksssksksss' + str(idx) + '.xml'
+        img_path = os.path.join("/images/", file_image)
+        label_path = os.path.join("/annotations/", file_label)
+        img = Image.open(img_path).convert("RGB")
+        # Generate Label
+        target = generate_target(idx, label_path)
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
